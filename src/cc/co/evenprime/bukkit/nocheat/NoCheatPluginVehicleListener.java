bc5985628e2859cf10e477f2097ac2f77277c1c1
Evenprime <Evenprime@example.com>
2011-02-17 21:48:32 +0100

    Forgot a file

---
+++ src/cc/co/evenprime/bukkit/nocheat/NoCheatPluginVehicleListener.java
@@ -0,0 +1,32 @@
+package cc.co.evenprime.bukkit.nocheat;
+
+
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.event.vehicle.VehicleListener;
+
+/**
+ * Handle events for all Player related events
+ * @author Evenprime
+ */
+
+public class NoCheatPluginVehicleListener extends VehicleListener {
+	
+    private final NoCheatPlugin plugin;
+    private final NoCheatPluginPlayerListener playerListener;
+    
+    public NoCheatPluginVehicleListener(NoCheatPlugin plugin, NoCheatPluginPlayerListener playerListener) {
+    	this.plugin = plugin;
+    	this.playerListener = playerListener;
+    }
+   	
+    @Override
+    public void onVehicleExit(VehicleExitEvent event) {
+    	playerListener.vehicleUpdate(event.getExited());
+    	
+    }
+    
+    public void onVehicleDamage(VehicleDamageEvent event) {
+    	playerListener.vehicleUpdate(event.getVehicle().getPassenger());
+    }
+}
