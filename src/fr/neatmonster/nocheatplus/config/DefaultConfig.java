f5eb4ca16c30cda5ecddcef53d38f55381692a3f
asofold <asofold@web.de>
2012-10-25 01:15:07 +0200

    Last config change in build 176.

--- src/fr/neatmonster/nocheatplus/config/DefaultConfig.java
+++ src/fr/neatmonster/nocheatplus/config/DefaultConfig.java
@@ -22,7 +22,7 @@ import org.bukkit.Material;
 public class DefaultConfig extends ConfigFile {
     
     /** NCP build needed for this config. */
-    public static final int buildNumber = 173;
+    public static final int buildNumber = 176;
 
     /**
      * Instantiates a new default configuration.
