ae5427e01d17851c7720e896c4261d41b9e88391
asofold <asofold@web.de>
2012-11-07 07:03:11 +0100

    Add Method for matching CheckType names [might get moved, changed arbitrarily].

--- src/fr/neatmonster/nocheatplus/command/CommandUtil.java
+++ src/fr/neatmonster/nocheatplus/command/CommandUtil.java
@@ -1,9 +1,15 @@
 package fr.neatmonster.nocheatplus.command;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.craftbukkit.CraftServer;
+
+import fr.neatmonster.nocheatplus.checks.CheckType;
 
 public class CommandUtil {
 	
@@ -29,5 +35,31 @@ public class CommandUtil {
 		final SimpleCommandMap map = getCommandMap();
 		final String lcAlias = alias.trim().toLowerCase();
 		return map.getCommand(lcAlias);
+	}
+
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static List<String> getCheckTypeTabMatches(final String input) {
+		final String ref = input.toUpperCase().replace('-', '_').replace('.', '_');
+		final List<String> res = new ArrayList<String>();
+		for (final CheckType checkType : CheckType.values()){
+			final String name = checkType.name();
+			if (name.startsWith(ref)) res.add(name);
+		}
+		if (ref.indexOf('_') == -1){
+			for (final CheckType checkType : CheckType.values()){
+				final String name = checkType.name();
+				final String[] split = name.split("_", 2);
+				if (split.length > 1 && split[1].startsWith(ref)) res.add(name);
+			}
+		}
+		if (!res.isEmpty()){
+			Collections.sort(res);
+			return res;
+		}
+		return null;
 	}
 }
