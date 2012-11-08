138431c4131c59f6b8d039fea91e27e46bf1ec5a
asofold <asofold@web.de>
2012-11-08 10:49:04 +0100

    Add interface for named components.

---
+++ src/fr/neatmonster/nocheatplus/components/ComponentWithName.java
@@ -0,0 +1,10 @@
+package fr.neatmonster.nocheatplus.components;
+
+/**
+ * Interface for components that can be registered with a name.
+ * @author mc_dev
+ *
+ */
+public interface ComponentWithName {
+	public String getComponentName();
+}
