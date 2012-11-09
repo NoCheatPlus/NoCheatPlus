86695a8cb05dfb716783b5b839ea67caf463c98b
asofold <asofold@web.de>
2012-11-09 08:37:55 +0100

    Add LogUtil to bundle some static access methods.

---
+++ src/fr/neatmonster/nocheatplus/utilities/LogUtil.java
@@ -0,0 +1,140 @@
+package fr.neatmonster.nocheatplus.utilities;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+
+/**
+ * Static access methods for logging access (mostly uses the Bukkit logger).
+ * @author mc_dev
+ *
+ */
+public class LogUtil {
+	
+	
+	public static String toString(final Throwable t){
+		// TODO: Find the fastest way.
+		final StringWriter w = new StringWriter();
+		t.printStackTrace(new PrintWriter(w));
+		return w.toString();
+	}
+	
+	
+	public static void logInfo(final String msg) {
+		log(Level.INFO, msg);
+	}
+	
+	public static void logWarning(final String msg) {
+		log(Level.WARNING, msg);
+	}
+	
+	public static void logSevere(final String msg) {
+		log(Level.SEVERE, msg);
+	}
+	
+	public static void logInfo(final Throwable t) {
+		log(Level.INFO, toString(t));
+	}
+	
+	public static void logWarning(final Throwable t) {
+		log(Level.WARNING, toString(t));
+	}
+	
+	public static void logSevere(final Throwable t) {
+		log(Level.SEVERE, toString(t));
+	}
+	
+	public static void log(final Level level, final String msg) {
+		Bukkit.getLogger().log(level, msg);
+	}
+
+	/**
+	 * Schedule a message to be output by the bukkit logger at info level, scheduled as task for NoCheatPlus.
+	 * 
+	 * @param message
+	 * @return If scheduled successfully.
+	 */
+	public static boolean scheduleLogInfo(final String message) {
+		return scheduleLog(Level.INFO, message);
+	}
+
+	/**
+	 * Schedule a message to be output by the bukkit logger at warning level, scheduled as task for NoCheatPlus.
+	 * 
+	 * @param message
+	 * @return If scheduled successfully.
+	 */
+	public static boolean scheduleLogWarning(final String message) {
+		return scheduleLog(Level.WARNING, message);
+	}
+
+	/**
+	 * Schedule a message to be output by the bukkit logger at warning level, scheduled as task for NoCheatPlus.
+	 * 
+	 * @param message
+	 * @return If scheduled successfully.
+	 */
+	public static boolean scheduleLogSevere(final String message) {
+		return scheduleLog(Level.SEVERE, message);
+	}
+	
+	public static boolean scheduleLogInfo(final Throwable t) {
+		return scheduleLog(Level.INFO, toString(t));
+	}
+	
+	public static boolean scheduleLogWarning(final Throwable t) {
+		return scheduleLog(Level.WARNING, toString(t));
+	}
+	
+	public static boolean scheduleLogSevere(final Throwable t) {
+		return scheduleLog(Level.SEVERE, toString(t));
+	}
+
+	/**
+	 * Schedule a log message with given level for the Bukkit logger with the NoCheatPlus plugin.<br>
+	 * 
+	 * @param level
+	 * @param message
+	 * @return
+	 */
+	public static boolean scheduleLog(final Level level, final String message) {
+		try {
+			return Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("NoCheatPlus"), new Runnable() {
+				@Override
+				public final void run() {
+					Bukkit.getLogger().log(level, message);
+				}
+			}) != -1;
+		} catch (final Exception exc) {
+			return false;
+		}
+	}
+	
+	/**
+	 * Schedule joined parts for info level.
+	 * @param level
+	 * @param parts
+	 * @param link
+	 * @return
+	 */
+	public static <O extends Object> boolean scheduleLogInfo(final List<O> parts, final String link)
+	{
+		return scheduleLog(Level.INFO, parts, link);
+	}
+	
+	/**
+	 * Schedule joined.
+	 * @param level
+	 * @param parts
+	 * @param link
+	 * @return
+	 */
+	public static <O extends Object> boolean scheduleLog(final Level level, final List<O> parts, final String link)
+	{
+		return scheduleLog(level, CheckUtils.join(parts, link));
+	}
+
+}
