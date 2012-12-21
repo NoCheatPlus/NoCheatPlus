3d773c694421f14fc067696eca77cb05f1faf11c
asofold <asofold@web.de>
2012-12-21 10:44:28 +0100

    [INSTABLE] Add support for MC1.4.6.

--- NCPPlugin/src/main/java/fr/neatmonster/nocheatplus/compat/MCAccessFactory.java
+++ NCPPlugin/src/main/java/fr/neatmonster/nocheatplus/compat/MCAccessFactory.java
@@ -2,6 +2,7 @@ package fr.neatmonster.nocheatplus.compat;
 
 import fr.neatmonster.nocheatplus.compat.cb2511.MCAccessCB2511;
 import fr.neatmonster.nocheatplus.compat.cb2512.MCAccessCB2512;
+import fr.neatmonster.nocheatplus.compat.cb2512.MCAccessMC1_4_6;
 
 /**
  * Factory class to hide potentially dirty stuff.
@@ -24,7 +25,10 @@ public class MCAccessFactory {
 			return new MCAccessCB2512();
 		}
 		catch(Throwable t){};
-		
+		try{
+			return new MCAccessMC1_4_6();
+		}
+		catch(Throwable t){};
 		throw new RuntimeException("Could not set up access to Minecraft API.");
 	}
 }
