5ca6e33fac248f5ed133dc62b7657e62050b9c71
asofold <asofold@web.de>
2013-02-28 20:26:24 +0100

    Reset lastAccess and lastExpired on clear().

--- NCPPlugin/src/main/java/fr/neatmonster/nocheatplus/checks/chat/analysis/engine/EnginePlayerDataMap.java
+++ NCPPlugin/src/main/java/fr/neatmonster/nocheatplus/checks/chat/analysis/engine/EnginePlayerDataMap.java
@@ -49,12 +49,14 @@ public class EnginePlayerDataMap extends ManagedMap<String, EnginePlayerData> {
 	}
 	
 	public void clear(){
-		for (ValueWrap wrap : map.values()){
+		final long time = System.currentTimeMillis();
+		for (final ValueWrap wrap : map.values()){
 			for (final WordProcessor processor : wrap.value.processors){
 				processor.clear();
 			}
 		}
 		super.clear();
+		lastAccess = lastExpired = time;
 	}
 
 	/* (non-Javadoc)
