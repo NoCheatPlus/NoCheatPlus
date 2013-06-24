a4556667fdeca3486538d674381fb356bb67fb70
asofold <asofold@web.de>
2013-06-24 12:24:36 +0200

    Add dedicated 1.5.2 module.

--- NCPPlugin/src/main/java/fr/neatmonster/nocheatplus/compat/MCAccessFactory.java
+++ NCPPlugin/src/main/java/fr/neatmonster/nocheatplus/compat/MCAccessFactory.java
@@ -10,7 +10,7 @@ import fr.neatmonster.nocheatplus.compat.cb2545.MCAccessCB2545;
 import fr.neatmonster.nocheatplus.compat.cb2602.MCAccessCB2602;
 import fr.neatmonster.nocheatplus.compat.cb2645.MCAccessCB2645;
 import fr.neatmonster.nocheatplus.compat.cb2691.MCAccessCB2691;
-import fr.neatmonster.nocheatplus.compat.cbdev.MCAccessCBDev;
+import fr.neatmonster.nocheatplus.compat.cb2763.MCAccessCB2763;
 import fr.neatmonster.nocheatplus.config.ConfPaths;
 import fr.neatmonster.nocheatplus.config.ConfigManager;
 import fr.neatmonster.nocheatplus.logging.LogUtil;
@@ -51,14 +51,22 @@ public class MCAccessFactory {
 			
 			// TEST //
 			// Only add as long as no stable module has been added.
+//			// 1.5.2
+//			try{
+//				return new MCAccessCBDev();
+//			}
+//			catch(Throwable t){
+//				throwables.add(t);
+//			};
+			// TEST END //
+			
 			// 1.5.2
 			try{
-				return new MCAccessCBDev();
+				return new MCAccessCB2763();
 			}
 			catch(Throwable t){
 				throwables.add(t);
 			};
-			// TEST END //
 			
 			// 1.5.1 (cb beta)
 			try{
