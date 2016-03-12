c5bda0674509ff58373cce0d2c569ba0da54c029
asofold <asofold@web.de>
2016-03-12 22:52:17 +0100

    Prioritize flying over elytra.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/MovingConfig.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/MovingConfig.java
@@ -349,6 +349,9 @@ public class MovingConfig extends ACheckConfig {
             default:
                 return modelGameMode;
         }
+        if (player.isFlying() || !ignoreAllowFlight && player.getAllowFlight()) {
+            return modelGameMode;
+        }
         // TODO: ORDER IS RANDOM GUESSING. Is mixtures possible?
         if (fromLocation.isInLiquid()) {
             // TODO: INCONSISTENT.
