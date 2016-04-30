f9d0a1a6ca588842c53f81dfa0c42a58282b4eaa
asofold <asofold@web.de>
2016-04-30 17:37:27 +0200

    [BLEEDING][INSTABLE] Something for boats.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/magic/MagicVehicle.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/magic/MagicVehicle.java
@@ -10,7 +10,7 @@ public class MagicVehicle {
 
     // Maximum distances per tick are wild guessing or from the wiki. Meant for extreme move checking, disregard velocity and effects. //
     // TODO: Configuration, once certain.
-    
+
     /** Extreme value. */
     public static final double maxDistanceHorizontal = 4.0; // TODO: 2.5 / 3.4?
 
@@ -25,5 +25,21 @@ public class MagicVehicle {
     public static final double donkeyMaxDistanceHorizontal = maxDistanceHorizontal; // 7.7 / 20.0; // TODO: 
     public static final double pigMaxDistanceHorizontal = maxDistanceHorizontal; // 8.2 / 20.0; // TODO: Doesn't fire anything.
     public static final double livingEntityMaxDistanceHorizontal = maxDistanceHorizontal; // (No idea, dragons?)
+
+
+    // Vertical distances //
+
+    /** Maximum descend distance. */
+    public static final double maxDescend = 5.0;
+    /** Maximum ascend distance (overall, no special effects counted in). */
+    // TODO: Likely needs adjustments, many thinkable edge cases, also pistons (!).
+    // TODO: Does trigger on vehicle enter somehow some time.
+    public static final double maxAscend = 0.27;
+
+    public static final double boatGravityMin = Magic.GRAVITY_MIN / 2.0;
+    public static final double boatGravityMax = Magic.GRAVITY_MAX + Magic.GRAVITY_SPAN;
+    /** The speed up to which gravity mechanics roughly work. */
+    public static final double boatVerticalFallTarget = 3.7;
+    public static final double boatMaxBackToSurfaceAscend = 1.5;
 
 }
--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/vehicle/VehicleEnvelope.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/vehicle/VehicleEnvelope.java
@@ -50,6 +50,9 @@ public class VehicleEnvelope extends Check {
     /** Tags for checks. */
     private final List<String> tags = new LinkedList<String>();
 
+    /** Extra details to log on debug. */
+    private final List<String> debugDetails = new LinkedList<String>();
+
     public VehicleEnvelope() {
         super(CheckType.MOVING_VEHICLE_ENVELOPE);
     }
@@ -58,6 +61,9 @@ public class VehicleEnvelope extends Check {
         // Delegate to a sub-check.
         tags.clear();
         tags.add("entity." + vehicle.getType());
+        if (data.debug) {
+            debugDetails.clear();
+        }
         final boolean violation;
         if (vehicle instanceof LivingEntity) {
             // TODO: No events (neither player move nor vehicle move). Flying packets with look.
@@ -65,6 +71,9 @@ public class VehicleEnvelope extends Check {
         }
         else {
             violation = checkEntity(player, vehicle, from, to, isFake, data, cc);
+        }
+        if (data.debug && !debugDetails.isEmpty()) {
+            debugDetails(player);
         }
         if (violation) {
             // Add up one for now.
@@ -80,6 +89,21 @@ public class VehicleEnvelope extends Check {
             data.vehicleSetBacks.setSafeMediumEntry(to); // TODO: Set only if it is safe to set. Set on monitor rather.
         }
         return null;
+    }
+
+    private void debugDetails(final Player player) {
+        if (!tags.isEmpty()) {
+            debugDetails.add("tags:");
+            debugDetails.add(StringUtil.join(tags, "+"));
+        }
+        final StringBuilder builder = new StringBuilder(500);
+        builder.append("Details:\n");
+        for (final String detail : debugDetails) {
+            builder.append(" , ");
+            builder.append(detail);
+        }
+        debug(player, builder.toString());
+        debugDetails.clear();
     }
 
     private boolean checkLivingEntity(final Player player, final LivingEntity vehicle, final Location from, final Location to, final boolean isFake, final MovingData data, final MovingConfig cc) {
@@ -166,12 +190,132 @@ public class VehicleEnvelope extends Check {
             final RichEntityLocation from, final RichEntityLocation to, 
             final VehicleMoveData thisMove, 
             final boolean isFake, final MovingData data, final MovingConfig cc) {
+        //        boolean violation = false;
+        if (data.debug) {
+            debugDetails.add("inair: " + data.sfJumpPhase);
+        }
         // Maximum thinkable horizontal speed.
         if (maxDistHorizontal(thisMove, MagicVehicle.boatMaxDistanceHorizontal)) {
             return true;
         }
-        // TODO: water, ice, ground, air, transitions
-        onNotHandle(vehicle);
+        // Maximum descend speed.
+        if (thisMove.vDistance < -MagicVehicle.maxDescend) {
+            // TODO: At times it looks like one move is skipped, resulting in double distance ~ -5 and at the same time 'vehicle moved too quickly'. 
+            // TODO: Test with log this to console to see the order of things.
+            tags.add("descend_much");
+            return true;
+        }
+        // Medium dependent checking.
+        boolean checkAscendMuch = true;
+        // (Assume boats can't climb.)
+        final boolean fromIsSafeMedium = from.isInWater() || from.isOnGround() || from.isInWeb();
+        final boolean toIsSafeMedium = to.isInWater() || to.isOnGround() || to.isInWeb();
+        if (from.isInWeb()) {
+            // TODO: Check anything?
+            if (data.debug) {
+                debugDetails.add("");
+            }
+            //            if (thisMove.vDistance > 0.0) {
+            //                tags.add("ascend_web");
+            //                return true;
+            //            }
+        }
+        else if (from.isInWater() && to.isInWater()) {
+            // Default in-medium move.
+            if (data.debug) {
+                debugDetails.add("water-water");
+            }
+            // TODO: Should still cover extreme moves here.
+
+            // Special case moving up after falling.
+            // TODO: Check past moves for falling (not yet available).
+            // TODO: Check if the target location somehow is the surface.
+            if (thisMove.vDistance > MagicVehicle.maxAscend && thisMove.vDistance < MagicVehicle.boatMaxBackToSurfaceAscend) {
+                // (Assume players can't control sinking boats for now.)
+                checkAscendMuch = false;
+                tags.add("back_to_surface");
+            }
+        }
+        else if (from.isOnGround() && to.isOnGround()) {
+            // Default on-ground move.
+            // TODO: Should still cover extreme moves here.
+            if (from.isOnIce() && to.isOnIce()) {
+                // Default on-ice move.
+                if (data.debug) {
+                    debugDetails.add("ice-ice");
+                }
+                // TODO: Should still cover extreme moves here.
+            }
+            else {
+                // (TODO: actually a default on-ground move.)
+                if (data.debug) {
+                    debugDetails.add("ground-ground");
+                }
+            }
+        }
+        else if (!fromIsSafeMedium && !toIsSafeMedium) {
+            // In-air move.
+            // TODO: Common in-air accounting check with parameters.
+            if (data.debug) {
+                debugDetails.add("air-air");
+            }
+            if (thisMove.vDistance > 0.0) {
+                tags.add("ascend_at_all");
+                return true;
+            }
+            // Absolute vertical distance to set back.
+            // TODO: Add something like this.
+            //            final double setBackYdistance = to.getY() - data.vehicleSetBacks.getValidSafeMediumEntry().getY();
+            //            if (data.sfJumpPhase > 4) {
+            // TODO: Realistic cap.
+            //                double estimate = Math.min(2.0, MagicVehicle.boatGravityMin * ((double) data.sfJumpPhase / 4.0) * ((double) data.sfJumpPhase / 4.0 + 1.0) / 2.0);
+            //                if (setBackYdistance > -estimate) {
+            //                    tags.add("slow_fall_vdistsb");
+            //                    return true;
+            //                }
+            //            }
+            // Slow falling (vdist).
+            if (data.sfJumpPhase > 1 && thisMove.vDistance > -MagicVehicle.boatVerticalFallTarget
+                    && thisMove.vDistance > - MagicVehicle.boatGravityMin * data.sfJumpPhase) {
+                tags.add("slow_fall_vdist");
+                return true;
+            }
+            // Fast falling (vdist).
+            if (data.sfJumpPhase > 1 && thisMove.vDistance < -MagicVehicle.boatGravityMax * data.sfJumpPhase - 0.5) {
+                tags.add("fast_fall_vdist");
+                return true;
+            }
+        }
+        else {
+            // Some transition to probably handle.
+            if (data.debug) {
+                debugDetails.add("?-?");
+            }
+            // TODO: Something needed here?
+            if (!toIsSafeMedium) {
+                // TODO: At least do something here?
+            }
+        }
+        // Maximum ascend speed.
+        if (checkAscendMuch && thisMove.vDistance > MagicVehicle.maxAscend) {
+            tags.add("ascend_much");
+            return true;
+        }
+
+        // No violation.
+        // TODO: sfJumpPhase is abused for in-air move counting here.
+        if (toIsSafeMedium) {
+            data.vehicleSetBacks.setSafeMediumEntry(to);
+            data.sfJumpPhase = 0;
+        }
+        else if (fromIsSafeMedium) {
+            data.vehicleSetBacks.setSafeMediumEntry(from);
+            data.sfJumpPhase = 0;
+        }
+        else {
+            data.sfJumpPhase ++;
+        }
+        data.vehicleSetBacks.setLastMoveEntry(to);
         return false;
     }
 
