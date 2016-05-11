a41ff38c997bcca780da32681e92216880e9e1b0
asofold <asofold@web.de>
2016-05-11 21:44:54 +0200

    Attempt to fix cross-plugin StackOverflowError with inventory.open.
    Not sure who started this, but apparently...
    * NCP closes an open inventory, leading to an event for that.
    * Due to the player having an item on the curser or similar, an item
    drop event is fired.
    * WorldGuard will kick the player due to a blacklist event.
    * NCP will detect an open inventory and attempt to close it, resulting
    in looping this.
    
    Fix attempt (blind) stores the uuid of a player and skips further nested
    closing of inventories.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/Open.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/Open.java
@@ -1,5 +1,7 @@
 package fr.neatmonster.nocheatplus.checks.inventory;
 
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
 
 import fr.neatmonster.nocheatplus.checks.Check;
@@ -13,44 +15,53 @@ import fr.neatmonster.nocheatplus.utilities.InventoryUtil;
  *
  */
 public class Open extends Check implements DisableListener{
-	
-	private static Open instance = null;
-	
-	// TODO: Add specific contexts (allow different settings for fight / blockbreak etc.).
-	
-	/**
-	 * Static access check, if there is a cancel-action flag the caller should have stored that locally already and use the result to know if to cancel or not.
-	 * @param player
-	 * @return If cancelling some event is opportune (open inventory and cancel flag set).
-	 */
-	public static boolean checkClose(Player player) {
-		return instance.check(player);
-	}
 
-	public Open() {
-		super(CheckType.INVENTORY_OPEN);
-		instance = this;
-	}
+    private static Open instance = null;
 
-	@Override
-	public void onDisable() {
-		instance = null;
-	}
-	
-	/**
-	 * This check contains the isEnabled checking (!). Inventory is closed if set in the config.
-	 * @param player
-	 * @return If cancelling some event is opportune (open inventory and cancel flag set).
-	 */
-	public boolean check(final Player player) {
-		if (!isEnabled(player) || !InventoryUtil.hasInventoryOpen(player)) {
-			return false;
-		}
-		final InventoryConfig cc = InventoryConfig.getConfig(player);
-		if (cc.openClose) {
-			player.closeInventory();
-		}
-		return cc.openCancelOther;
-	}
+    private UUID nestedPlayer = null;
+
+    // TODO: Add specific contexts (allow different settings for fight / blockbreak etc.).
+
+    /**
+     * Static access check, if there is a cancel-action flag the caller should have stored that locally already and use the result to know if to cancel or not.
+     * @param player
+     * @return If cancelling some event is opportune (open inventory and cancel flag set).
+     */
+    public static boolean checkClose(Player player) {
+        return instance.check(player);
+    }
+
+    public Open() {
+        super(CheckType.INVENTORY_OPEN);
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+        nestedPlayer = null;
+    }
+
+    /**
+     * This check contains the isEnabled checking (!). Inventory is closed if set in the config.
+     * @param player
+     * @return If cancelling some event is opportune (open inventory and cancel flag set).
+     */
+    public boolean check(final Player player) {
+        if (!isEnabled(player) || !InventoryUtil.hasInventoryOpen(player)) {
+            return false;
+        }
+        final InventoryConfig cc = InventoryConfig.getConfig(player);
+        if (cc.openClose) {
+            final UUID id = player.getUniqueId();
+            if (this.nestedPlayer == null || !id.equals(this.nestedPlayer)) {
+                // (The second condition represents an error, but we don't handle alternating things just yet.)
+                this.nestedPlayer = id;
+                player.closeInventory();
+                this.nestedPlayer = null;
+            }
+        }
+        return cc.openCancelOther;
+    }
 
 }
