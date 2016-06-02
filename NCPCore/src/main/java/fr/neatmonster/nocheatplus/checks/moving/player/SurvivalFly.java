4e91a9d5fcdc140954bfcd82f04ff237b1e4bb34
asofold <asofold@web.de>
2016-06-02 20:14:09 +0200

    Ready for UEFA EC: GPLv3 headers.
    Might need more training still.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/player/SurvivalFly.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/player/SurvivalFly.java
@@ -1,3 +1,17 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.neatmonster.nocheatplus.checks.moving.player;
 
 import java.util.ArrayList;
@@ -540,6 +554,7 @@ public class SurvivalFly extends Check {
         // TODO: Hide and seek with API.
         // TODO: Pull down tick / timing data (perhaps add an API object for millis + source + tick + sequence count (+ source of sequence count).
         if (data.debug) {
+            // TODO: Only update, if velocity is queued at all.
             data.getVerticalVelocityTracker().updateBlockedState(TickTask.getTick(), 
                     // Assume blocked with being in web/water, despite not entirely correct.
                     thisMove.headObstructed || thisMove.from.resetCond,
