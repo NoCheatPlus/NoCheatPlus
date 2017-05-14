416a2d4ef3c5d375ed22f885df94085128b866f6
asofold <asofold@web.de>
2017-05-14 13:40:24 +0200

    Extend the capabilities of the 'ncp log stream ...' command.
    * Allow specifying to remove color definitions (both Minecraft default
    and with '&') and to replace color definitions that use '&'. Append
    ?color or ?nocolor after the level definition. (Shortcuts: c, col / nc,
    noc, nocol, nocolor). Either of both can be set, not both.
    * Log to multiple streams with individual levels and color flags by
    separating stream definitions with '+'.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/log/stream/StreamCommand.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/log/stream/StreamCommand.java
@@ -2,6 +2,7 @@ package fr.neatmonster.nocheatplus.command.admin.log.stream;
 
 import java.util.logging.Level;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +12,7 @@ import fr.neatmonster.nocheatplus.command.BaseCommand;
 import fr.neatmonster.nocheatplus.logging.LogManager;
 import fr.neatmonster.nocheatplus.logging.StreamID;
 import fr.neatmonster.nocheatplus.logging.Streams;
+import fr.neatmonster.nocheatplus.utilities.ColorUtil;
 import fr.neatmonster.nocheatplus.utilities.StringUtil;
 
 /**
@@ -23,7 +25,7 @@ public class StreamCommand extends BaseCommand {
 
     public StreamCommand(JavaPlugin plugin) {
         super(plugin, "stream", null); // No permission: currently console-only.
-        this.usage = "ncp log stream stream_id[@level] (message...)";
+        this.usage = "ncp log stream stream_id[@level][?color|?nocolor][+stream_id2[@level2][?color|?nocolor]...] (message...) ";
     }
 
     @Override
@@ -34,56 +36,111 @@ public class StreamCommand extends BaseCommand {
         if (args.length < 4) {
             return false;
         }
-        String streamDef = args[2];
-        Level level = null;
-        if (streamDef.indexOf('@') != -1) {
-            String[] split = streamDef.split("@");
-            if (split.length != 2) {
-                return false;
-            }
-            streamDef = split[0];
-
-            // Attempt to parse level.
-            try {
-                // Convert to upper-case to ignore case.
-                level = Level.parse(split[1].toUpperCase());
-            }
-            catch (IllegalArgumentException e) {
-                sender.sendMessage("Bad level: " + split[1]);
-                return true;
-            }
-        }
         LogManager man = NCPAPIProvider.getNoCheatPlusAPI().getLogManager();
-        StreamID streamId = man.getStreamID(streamDef);
-        if (streamId == null) {
-            String altStreamDef = streamDef.toLowerCase();
-            if (altStreamDef.equals("notify")) {
-                // Default level should be INFO.
-                streamId = Streams.NOTIFY_INGAME;
-            }
-            else if (altStreamDef.equals("debug")) {
-                streamId = Streams.TRACE_FILE;
-                if (level == null) {
-                    level = Level.FINE;
+        String message = null;
+        String messageColor = null;
+        String messageNoColor = null;
+        for (String streamDef : args[2].split("\\+")) {
+            Level level = null;
+            boolean color = false;
+            boolean noColor = false;
+            // Check for color def.
+            if (streamDef.indexOf('?') != -1) {
+                String[] split = streamDef.split("\\?");
+                if (split.length != 2) {
+                    sender.sendMessage("Bad flag (color|nocolor): " + streamDef);
+                    continue;
+                }
+                streamDef = split[0];
+                String temp = split[1].toLowerCase();
+                if (temp.matches("^(nc|noc|nocol|nocolor)$")) {
+                    noColor = true;
+                }
+                else if (temp.matches("^(c|col|color)$")) {
+                    color = true;
+                }
+                else {
+                    sender.sendMessage("Bad flag (color|nocolor): " + temp);
+                    continue;
                 }
             }
-            else if (altStreamDef.equals("status")) {
-                streamId = Streams.STATUS;
+            // Parse level first.
+            if (streamDef.indexOf('@') != -1) {
+                String[] split = streamDef.split("@");
+                if (split.length != 2) {
+                    sender.sendMessage("Bad level definition: " + streamDef);
+                    continue;
+                }
+                streamDef = split[0];
+
+                // Attempt to parse level.
+                try {
+                    // Convert to upper-case to ignore case.
+                    level = Level.parse(split[1].toUpperCase());
+                }
+                catch (IllegalArgumentException e) {
+                    sender.sendMessage("Bad level: " + split[1]);
+                    continue;
+                }
             }
-            else if (altStreamDef.equals("init")) {
-                streamId = Streams.INIT;
+            // Get StreamID, account for shortcuts.
+            StreamID streamId = man.getStreamID(streamDef);
+            if (streamId == null) {
+                String altStreamDef = streamDef.toLowerCase();
+                if (altStreamDef.equals("notify")) {
+                    // Default level should be INFO.
+                    streamId = Streams.NOTIFY_INGAME;
+                }
+                else if (altStreamDef.equals("debug")) {
+                    streamId = Streams.TRACE_FILE;
+                    if (level == null) {
+                        level = Level.FINE;
+                    }
+                }
+                else if (altStreamDef.equals("status")) {
+                    streamId = Streams.STATUS;
+                }
+                else if (altStreamDef.equals("init")) {
+                    streamId = Streams.INIT;
+                }
+                else if (altStreamDef.equals("console")) {
+                    // Prefer the plugin logger.
+                    streamId = Streams.PLUGIN_LOGGER;
+                }
+                else if (altStreamDef.equals("file")) {
+                    streamId = Streams.DEFAULT_FILE;
+                }
+                else {
+                    sender.sendMessage("Bad stream id: " + streamDef);
+                    continue;
+                }
+            }
+            // Finally log.
+            if (level == null) {
+                // Instead: context-dependent?
+                level = Level.INFO;
+            }
+            if (message == null) {
+                message = StringUtil.join(args, 3, " ");
+            }
+            final String logMessage;
+            if (noColor) {
+                if (messageNoColor == null) {
+                    messageNoColor = ChatColor.stripColor(ColorUtil.removeColors(message));
+                }
+                logMessage = messageNoColor;
+            }
+            else if (color) {
+                if (messageColor == null) {
+                    messageColor = ColorUtil.replaceColors(message);
+                }
+                logMessage = messageColor;
             }
             else {
-                sender.sendMessage("Bad stream id: " + streamDef);
-                return true;
+                logMessage = message;
             }
+            man.log(streamId, level, logMessage);
         }
-        if (level == null) {
-            // Instead: context-dependent?
-            level = Level.INFO;
-        }
-        String message = StringUtil.join(args, 3, " ");
-        man.log(streamId, level, message);
         // (No success message.)
         return true;
     }
