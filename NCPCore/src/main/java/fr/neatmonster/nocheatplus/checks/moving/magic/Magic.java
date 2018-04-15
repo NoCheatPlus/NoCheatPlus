f079b5226754f76adbb97c179e035fc11bc5faae
asofold <asofold@web.de>
2018-04-15 19:21:55 +0200

    Allow dealing fall damage from 3.5 blocks height on.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/magic/Magic.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/magic/Magic.java
@@ -97,7 +97,7 @@ public class Magic {
     /** The lower bound of fall distance for taking fall damage. */
     public static final double FALL_DAMAGE_DIST = 3.0;
     /** The minimum damage amount that actually should get applied. */
-    public static final double FALL_DAMAGE_MINIMUM = 1.0;
+    public static final double FALL_DAMAGE_MINIMUM = 0.5;
 
     /**
      * The maximum distance that can be achieved with bouncing back from slime
