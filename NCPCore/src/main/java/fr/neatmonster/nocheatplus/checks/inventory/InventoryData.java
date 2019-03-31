70a745933ced0721380e1f4008f6f7990ea9bd84
CaptainObvious0 <michaelstets1@gmail.com>
2019-03-31 16:37:04 -0500

    InventoryData - chestOpenTime
    Adds the variable to check when a player opens a chest

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryData.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryData.java
@@ -34,6 +34,7 @@ public class InventoryData extends ACheckData {
 
     // General.
     public long     lastClickTime = 0;
+    public long     chestOpenTime = 0;
 
     // Data of the drop check.
     public int      dropCount;
